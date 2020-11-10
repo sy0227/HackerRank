f4ea7183694284e8c45672f578c8b9f5f5110496
sy0227 <tv0227@naver.com>
2020-11-10 12:07:18 +0900

    Create Arrays-DS.py

---
+++ Data Structure/Arrays-DS.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    result = []
+    i = len(a)
+    while(i>0) :
+        i -= 1
+        result.append(a[i])
+    return result
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
