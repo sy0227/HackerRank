7940c12e65d542a07f7bac9a7e1b654e9e2dbe4b
sy0227 <tv0227@naver.com>
2020-11-10 11:59:27 +0900

    Create New_Companies.sql

---
+++ SQL/New_Companies.sql
@@ -0,0 +1,8 @@
+select company_code, founder, 
+    (select count(distinct lead_manager_code) from Lead_Manager where company_code=C.company_code),
+    (select count(distinct senior_manager_code) from Senior_Manager where company_code=C.company_code),
+    (select count(distinct manager_code) from Manager where company_code=C.company_code),
+    (select count(distinct employee_code) from Employee where company_code=C.company_code)
+    
+from Company C
+order by company_code ASC;
